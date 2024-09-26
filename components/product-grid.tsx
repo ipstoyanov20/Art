35b83f0bcaf9b0ea202882dbc9615f38916bb1dd
Ivaylo Stoyanov <ipstoyanov20@codingburgas.bg>
2024-09-26 22:20:40 +0300

    Fix typeerror

--- components/product-grid.tsx
+++ components/product-grid.tsx
@@ -30,12 +30,22 @@ export function ProductGrid({ products }: Props) {
   return (
     <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-3 lg:col-span-3 lg:gap-x-8">
       {products.map((product) => (
-        <Link key={product._id} href={`/products/${product.slug}`} className="group text-sm">
+        <Link
+          key={product._id}
+          href={`/products/${product.slug}`}
+          className="group text-sm"
+        >
           <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg border-2 border-gray-200 bg-gray-100 group-hover:opacity-75 dark:border-gray-800">
             <Image
               placeholder="blur"
-              blurDataURL={`data:image/svg+xml;base64,${toBase64( shimmer(225, 280) )}`}
-              src={urlForImage(product.images[0]).url()}
+              blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                shimmer(225, 280)
+              )}`}
+              src={
+                product.images?.[0]
+                  ? urlForImage(product.images[0])?.url()
+                  : "/path/to/placeholder/image.jpg"
+              }
               alt={product.name}
               width={225}
               height={280}
@@ -43,7 +53,14 @@ export function ProductGrid({ products }: Props) {
             />
           </div>
           <h3 className="mt-4 font-medium">{product.name}</h3>
-          <p className="mt-2 font-medium">{formatCurrencyString({currency: product.currency, value: product.price})}</p>
+          <p className="mt-2 font-medium">
+            {product.currency && product.price
+              ? formatCurrencyString({
+                  currency: product.currency,
+                  value: product.price,
+                })
+              : "Price unavailable"}
+          </p>{" "}
         </Link>
       ))}
     </div>
